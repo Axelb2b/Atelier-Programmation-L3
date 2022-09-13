eadecf5d4ac969913cfcd957ff4fdba2878c55fd
Axelb2b <brunelaxel@gmail.com>
2022-09-13 09:19:00 +0200

    Atelier 3

---
+++ Atelier 3/Ex1.py
@@ -0,0 +1,63 @@
+
+
+
+def full_name(nom_prenom : str) -> str:
+    """Renvoie le nom en majuscules et le prénom avec la première lettre en majuscule
+
+    Args:
+        nom_prenom (str): Nom puis Prénom
+
+    Returns:
+        str: NOM Prénom
+    """
+    TAB = nom_prenom.split(" ")
+    nom = TAB[0].upper()
+    prenom = TAB[1].capitalize()
+    return nom+" "+prenom
+
+def is_mail(mail : str) -> tuple:
+    """Verifie à travers plusierus conditions si un mail  est valide 
+
+    Args:
+        mail (str): un mail
+
+    Returns:
+        tuple: couple d'indices selon l'erreur (si il y en a une)
+    """
+    tup = (0,0)
+    #on sépare ce qu'ily a avant et après le @ 
+    TAB_MAIL = mail.split("@")
+    print(TAB_MAIL[0])
+    #Si il n'y a pas de @ , retorune un tuple erreur
+    if len(TAB_MAIL) < 2:
+        return (0,2)
+    #Si le corps est vide ou si il y a un . au début ou à la fin ou ".."  ou si il y a un caractère non autorisé retourne un tuple erreur 
+    if TAB_MAIL[0] != "" and (TAB_MAIL[0][0] and TAB_MAIL[0][-1]) != "." and ".." not in TAB_MAIL[0] and ("/" not in TAB_MAIL[0] and "_" not in TAB_MAIL[0]):
+        #rel   atif au domaine 
+        TAB_DOM = TAB_MAIL[1].split(".")
+        #Si il y a exactement 1 point et qu'il n'est pas à la fin et qu'il n'y a pas de caractères non-autorisés retourne un tuble correct
+        if len(TAB_DOM) == 2 and TAB_DOM[1] != "" and("/" not in TAB_DOM[1] and "_" not in TAB_DOM[1]):
+            tup = (1,0)
+        else:
+            #si il n'y a pas de point retourne un tuple erreur
+            if len(TAB_DOM) == 1:
+                tup = (0,4)
+            else:
+                #retourne le dernier tuple erreur de domaine
+                tup = (0,3)
+
+    else:
+        tup = (0,1)
+    return tup
+
+
+
+
+
+
+print(is_mail("brunelaxel@univ-corse.fr"))
+print(is_mail("brune/axel@gmail.com"))
+print(is_mail("brunelaxel.com"))
+print(is_mail("brunelaxel@Gmailcom"))
+print(is_mail("brunelaxel@gmail.co/m"))
+print(is_mail("g.@fht.fr"))
---
+++ Atelier 3/Ex2.py
@@ -0,0 +1,113 @@
+MOT = ["pédiluve","australopithèque","peripeptide","salvateur","mastication","oui","non","ok","bref","intraveineuse"]
+
+
+
+
+def mot_nlettre(lst : list,n : int) -> list:
+    lst_n = []
+    for e in lst:
+        if len(e) == n:
+            lst_n.append(e)
+    return lst_n
+
+
+
+def commence_par(mot : str, pre : str) -> bool:
+    """retourne un booleen si le prefixe en paramètre est dans le mot
+
+    Args:
+        mot (str): mot
+        pre (str): préfixe
+
+    Returns:
+        bool: prefixe du mot ou pas 
+    """
+    pre_mot = ""
+    for i in range(len(pre)):
+        pre_mot += mot[i]
+    if pre_mot == pre:
+        return True
+    else:
+        return False
+
+def finit_par(mot : str,suff : str) ->bool:
+    """retourne un booleen si le suffixe du mot etst le même que celui passé en paramètre
+
+    Args:
+        mot (str): mot
+        suff (str): suffixe
+
+    Returns:
+        bool: si le préfixe est dedans ou pas
+    """
+    suff_mot = ""
+    for i in reversed(range(len(suff))):
+        suff_mot += mot[-1-i]
+    if suff_mot == suff:
+        return True
+    else:
+        return False
+
+
+def finissent_par(lst_MOT : list, suff : str) -> list:
+    """utilise finit_par() pour trouver les mots d'une liste qui finissent par un suffixe passé en paramètre
+
+    Args:
+        lst_MOT (list): liste de mot
+        suff (str): suffixe recherché
+
+    Returns:
+        list: liste contenant les mots avec le suffixe passé en paramètre
+    """
+    MOT = []
+    for e in lst_MOT:
+        if finit_par(e,suff):
+            MOT.append(e)
+    return MOT
+
+def commencent_par(lst_mot : list, pre : str) -> list:
+    """idem que finissent_par() pour lespréfixes
+
+    Args:
+        lst_mot (list): liste de mot
+        pre (str): préfixe
+
+    Returns:
+        list: liste de mots contenant le préfixe passé en paramètre
+    """
+    MOT = []
+    for e in lst_mot:
+        if commence_par(e,pre):
+            MOT.append(e)
+    return MOT
+
+
+
+def liste_mot(lst_mot : list,pre : str,suff : str,n : int):
+    """combine tout pour vérifier si dans une liste de mot quels mots finissent par un prefixe ET un suffixe donnés et qui fait n lettres de long
+
+    Args:
+        lst_mot (list): liste de mots
+        pre (str): préfixe
+        suff (str): suffixe
+        n (int): longueur du mot
+
+    Returns:
+        _type_: liste de mos remplissant les conditions
+    """
+    lst_n = mot_nlettre(lst_mot,n)
+    lst_pre = commencent_par(lst_n,pre)
+    lst_tout = finissent_par(lst_pre,suff)
+    return lst_tout
+
+
+def dictionnaire(fichier):
+    f = open((fichier,"r"))
+    c = f.readline()
+    print("**LISTE DE MOTS**")
+    while c != "":
+        print(c)
+        c = f.readline()
+    print("**FINI**")
+
+dictionnaire(littre.txt)
